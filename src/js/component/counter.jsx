ee51037f0500bd6c3abefb9705631b61e0751436
Álvaro M. Villar <144016805+amv87@users.noreply.github.com>
2023-10-31 19:23:25 +0000

    added counter.jsx

---
+++ src/js/component/counter.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Counter = () => {
+	return (
+		<div className="d-flex align-items-center rounded justify-content-center border border-dark" style={{width: '8rem', height:'12rem', backgroundColor: 'rgb(22, 22, 22)'}}>
+				<number className="text-white display-1 fw-bold"></number>
+		</div>
+	);
+};
+
+export default Counter;
